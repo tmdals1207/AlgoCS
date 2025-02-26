7b2ea8685903e43ead6c4429764f552df1fb879a
Jin <149752689+kuru2141@users.noreply.github.com>
2025-02-26 15:10:24 +0900

    Update 양궁대회.py

--- 0303/양궁대회.py
+++ 0303/양궁대회.py
@@ -1,25 +1,22 @@
-list_arr = [] # 최종 n점 과녁에 맞춘 화살 수 배열
-score_arr = [] # 최종 점수를 담을 리스트
-
 def solution(n, info):
     global list_arr, score_arr
 
-    list_arr = [] # 초기화
-    score_arr = [] # 초기화
-    
+    list_arr = [] # 최종 n점 과녁에 맞춘 화살 수 배열의 배열
+    score_arr = [] # 최종 점수를 담을 배열
     result_arr = [0] * 11 # n점 과녁에 맞춘 화살 수 배열
+    
     dfs(0, result_arr, n, info)
 
     if list_arr:
         tmp = max(score_arr)
-        tmp_result = []
+        tmp_result = [] # 점수가 가장 높은 배열의 배열
         for i in range(len(score_arr)):
             if tmp == score_arr[i]:
                 tmp_result.append(list_arr[i])
 
-        while len(tmp_result) > 1:
+        while len(tmp_result) > 1: # 각 배열의 뒷 index부터 확인해서 작은 것 부터 pop
             for j in range(len(tmp_result[0]) - 1, 0, -1):
-                value = []
+                value = [] # 각 배열의 j번째 값을 넣을 배열
 
                 for i in range(len(tmp_result)):
                     value.append(tmp_result[i][j])
@@ -29,33 +26,34 @@ def solution(n, info):
                         tmp_result.remove(tmp_result[i])
                         if len(tmp_result) == 1:
                             break
-                            
-        result = tmp_result[-1]
+
+        result = tmp_result[0] # 마지막으로 남은 배열 출력
         
     else:
         result = [-1]
     return result
 
+
 def dfs(i, result_arr, n, info):
     global list_arr, score_arr
 
     if n == 0 or i == 11:
-        tmp_list = [0] * 11
+        tmp_list = [0] * 11 # 얻거나 뺏긴 점수를 입력할 배열
         
-        if n > 0:
+        if n > 0: # 쏘지 않은 화살이 있다면 마지막에 다 소모
             result_arr[-1] += n
 
-        for x in range(11):
+        for x in range(11): # 점수 세기
             if result_arr[x] > info[x]:
                 tmp_list[x] = 10 - x
             elif info[x] != 0:
                 tmp_list[x] = x - 10
 
-        if sum(tmp_list) > 0:
+        if sum(tmp_list) > 0: # 이긴 경우에만 배열에 추가
             list_arr.append(result_arr[:])
             score_arr.append(sum(tmp_list))
             
-        result_arr[-1] = 0
+        result_arr[-1] = 0 # 백트래킹
         return
 
     if n > info[i]:
