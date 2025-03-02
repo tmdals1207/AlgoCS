009b56d30f41438e723385bb464fd64b11045268
Jin <149752689+kuru2141@users.noreply.github.com>
2025-03-03 06:54:40 +0900

    Update 땅따먹기.py

--- 0303/땅따먹기.py
+++ 0303/땅따먹기.py
@@ -3,7 +3,7 @@ def solution(land):
         for j in range(4):
             tmp = land[i][j]
             for k in range(4):
-                if j != k:
+                if j != k: # land[i][j]와, land[i][j]에 이전 행의 숫자를 더한 것을 비교함으로써 가장 높은 값을 뽑음
                     land[i][j] = max(land[i][j], tmp + land[i - 1][k])
     
     return max(land[-1])
