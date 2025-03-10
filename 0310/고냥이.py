5d5f8013ba8d6f410baa917bb7e290eee2bd1af9
Jin <149752689+kuru2141@users.noreply.github.com>
2025-03-10 19:08:32 +0900

    Update 고냥이.py

--- 0310/고냥이.py
+++ 0310/고냥이.py
@@ -2,15 +2,15 @@ from collections import deque
 
 N = int(input())
 word = input()
+
 arr_queue = deque([e for e in word])
-max_conti = 0
 input_queue = deque()
 
-idx_list = [0]
-i = 1
-j = 1
-idx_sum = 0
-max_len = 1
+idx_list = [0] # 다른 문자가 들어올 때 인덱스 저장
+i = 1 # idx_list에서 사용
+j = 1 # 다른 문자가 들어왔을 때의 인덱스
+idx_sum = 0 # 
+max_len = 1 # 문자열의 최대 길이
 
 input_queue.append(arr_queue.popleft())
 
@@ -21,10 +21,11 @@ while arr_queue:
         idx_list.append(j)
         
         input_queue.append(ele)
-        tmp = set(input_queue)
+        tmp = set(input_queue) # 중복된 원소 제거
         
-        while len(tmp) > N:
+        while len(tmp) > N: # 다른 문자가 N개 보다 작아질 때까지 반복
             idx_sum = idx_list[i] - idx_list[i - 1]
+            # 다른 문자의 인덱스 차이만큼 원소 제거
             for _ in range(idx_sum):
                 input_queue.popleft()
             i += 1
