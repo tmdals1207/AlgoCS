40b1c9c964b0b2a6f1f896d168dfdc247b2ac35e
Choi Jeongmin <89333351+likerhythm@users.noreply.github.com>
2025-03-13 10:21:29 +0900

    Update 동물원.java

--- 0317/동물원.java
+++ 0317/동물원.java
@@ -17,11 +17,6 @@ public class Main {
         int[] dp1 = new int[N]; // dp1[i] : i행 사자를 놓을 때 사자를 배치하는 최대 경우의 수
         int[] dp2 = new int[N]; // dp2[i] : i행 사자를 놓지 않을 때 사자를 배치하는 최대 경우의 수
 
-        if (N == 1) {
-            System.out.println(3);
-            return;
-        }
-
         dp1[0] = 2; // 첫 열에 사자를 놓는 경우의 수
         dp2[0] = 1; // 첫 열에 사자를 놓지 않는 경우의 수
 
