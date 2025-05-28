9e5958552f30471f136f2202aa79575996eaa76c
yoon_je <67690073+etoile0626@users.noreply.github.com>
2025-05-28 14:47:28 +0900

    Update 세 용액.java

--- 0602/세 용액.java
+++ 0602/세 용액.java
@@ -24,15 +24,15 @@ public class Main_BJ2473 {                                    //세 용액
             arr[i] = Long.parseLong(st.nextToken());
         }
 
-        Arrays.sort(arr);
+        Arrays.sort(arr);                           //입력값 정렬
 
-        int[] sol = new int[3];
+        int[] sol = new int[3];                     //정답 인덱스를 담을 배열
 
         long min = Long.MAX_VALUE;
 
-        outer: for(int i = 0; i < n; i++){
+        outer: for(int i = 0; i < n; i++){          //for문을 통해 용액 하나 지정
             int left = 0;
-            int right = n-1;
+            int right = n-1;                        //투 포인터로 남은 2개 용액 지정
             while(left < right) {
                 if (left == i) {
                     left++;
@@ -41,28 +41,28 @@ public class Main_BJ2473 {                                    //세 용액
 
                 if (right == i) {
                     right--;
-                    continue;
-                }
+                    continue;                       //같은 용액을 고른 경우는 패스
+                }                                   
 
                 long sum = arr[i] + arr[left] + arr[right];
-                if(min > Math.abs(sum)){
+                if(min > Math.abs(sum)){            //최솟값 갱신
                     min = Math.abs(sum);
                     sol[0] = i;
                     sol[1] = left;
                     sol[2] = right;
                 }
 
-                if(sum > 0){
+                if(sum > 0){                        //포인터 이동
                     right--;
                 } else if(sum < 0){
                     left++;
-                } else{
+                } else{                             //세 용액 합이 0인 경우 즉시 탈출
                     break outer;
                 }
             }
         }
 
-        Arrays.sort(sol);
+        Arrays.sort(sol);                           //i, left, right를 오름차순으로 정렬
         System.out.println(arr[sol[0]] + " " + arr[sol[1]] + " " + arr[sol[2]]);
     }
 }
